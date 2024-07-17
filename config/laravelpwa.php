9eed9f5c787f9a5f08f1e7400b0b88bb8855ce06
macycumapy <kopperphild@gmail.com>
2024-07-17 18:54:00 +0500

    Настройки PWA

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -3,32 +3,27 @@
 declare(strict_types=1);
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'DCostsPWA',
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'short_name' => 'DCosts',
         'start_url' => '/',
         'background_color' => '#ffffff',
-        'theme_color' => '#000000',
+        'theme_color' => '#064E3B',
         'display' => 'standalone',
         'orientation' => 'any',
         'status_bar' => 'black',
         'icons' => [],
         'shortcuts' => [
             [
-                'name' => 'Shortcut Link 1',
-                'description' => 'Shortcut Link 1 Description',
-                'url' => '/shortcutlink1',
+                'name' => 'Отчет о затратах',
+                'description' => 'Отчет о затратах',
+                'url' => '/reports/outflows',
                 'icons' => [
                     "src" => "/images/logo.png",
                     "purpose" => "any"
                 ]
             ],
-            [
-                'name' => 'Shortcut Link 2',
-                'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
-            ]
         ],
         'custom' => []
     ]
