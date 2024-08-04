89278f7864fa4cace01296e17d516d00a69e59e8
macycumapy <kopperphild@gmail.com>
2024-08-04 17:53:38 +0500

    Добавлена светлая тема

--- resources/views/livewire/widget/theme-widget.blade.php
+++ resources/views/livewire/widget/theme-widget.blade.php
@@ -3,14 +3,14 @@
 }" x-init="
     Alpine.store('theme', theme)
     $watch('theme', (value) => Alpine.store('theme', value))
-" class="w-100 relative flex items-center cursor-pointer" wire:click="$toggle('isDark')">
+" class="w-100 relative flex items-center cursor-pointer">
     @if($isDark)
-        <x-icon name="sun" class="w-4 h-4 text-white absolute left-1 z-10"></x-icon>
+        <x-icon name="sun" class="w-4 h-4 text-white absolute left-1 z-10" wire:click="$toggle('isDark')"></x-icon>
     @endif
 
-    <x-toggle wire:model.live="isDark" name="toggle" xl />
+    <x-toggle wire:model="isDark" name="toggle" xl wire:click="$toggle('isDark')"/>
 
     @unless($isDark)
-        <x-icon name="moon" class="w-4 h-4 text-gray-400 absolute left-6 z-10"></x-icon>
+        <x-icon name="moon" class="w-4 h-4 text-gray-400 absolute left-6 z-10" wire:click="$toggle('isDark')"></x-icon>
     @endunless
 </div>
