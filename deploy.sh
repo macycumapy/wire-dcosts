e07a7a7163eccde44f8d9b80d2e2818c048ded4d
macycumapy <kopperphild@gmail.com>
2024-10-02 16:15:57 +0500

    CI/CD

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git pull
+./vendor/bin/sail composer install
+./vendor/bin/sail artisan migrate --seed
+./vendor/bin/sail artisan optimize:clear
+./vendor/bin/sail npm install
+./vendor/bin/sail npm run build
+
+
